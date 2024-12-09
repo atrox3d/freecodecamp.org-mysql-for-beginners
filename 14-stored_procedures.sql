5401aa85c82968734b71d0c0f93cc6d827c4002a
atrox3d <atrox3d@gmail.com>
2024-12-09 12:34:27 +0100

    add comments to stored procedure

--- 14-stored_procedures.sql
+++ 14-stored_procedures.sql
@@ -1,17 +1,17 @@
 drop procedure employee_count_dept;
 
-delimiter //
-create procedure if not exists employee_count_dept()
-begin
-    select department.dept_no, department.dept_name
+delimiter //                                        -- change delimiter
+create procedure employee_count_dept()              -- create sp
+begin                                               -- begin sql block
+    select department.dept_no, department.dept_name -- actual query
         , count(employee.emp_no) as num_employees
     from department
     left join employee
     on department.dept_no = employee.dept_no
-    group by department.dept_no;
--- from mysql terminal
-end //
-delimiter ;
+    group by department.dept_no;                    -- end query with ;
+end //                                              -- end sql block with //
+delimiter ;                                         -- restore delimiter
 
+-- call sp
 call employee_count_dept();
 
