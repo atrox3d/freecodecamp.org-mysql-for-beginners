62754da932b3d793c49ad20cd42ae0fddd3306b2
atrox3d <atrox3d@gmail.com>
2024-12-13 07:27:51 +0100

    update renumber, won't run automatically

--- renumber.py
+++ renumber.py
@@ -2,10 +2,12 @@ from pathlib import Path
 import re
 
 def splitext(filepath:str) -> tuple[str, str]:
+    ''' separate name, ext: same as os.path.splitext '''
     filepath:Path = Path(filepath)
     return filepath.stem, filepath.suffix
 
 def renumber(filename:str, add:int=1) -> str:
+    ''' renumbers string like 1-smtng to 02-smtng'''
     match = re.match(
         r'(\d+)-([\w-]+)',
         filename
@@ -13,13 +15,14 @@ def renumber(filename:str, add:int=1) -> str:
     num, name = match.groups()
     return f'{int(num)+add:02}-{name}'
 
-brocode = Path('brocode')
-assert brocode.exists()
-assert brocode.is_dir()
+def main():
+    brocode = Path('brocode')
+    assert brocode.exists()
+    assert brocode.is_dir()
 
-for script in sorted(brocode.glob('*.sql')):
-    print(script.name)
-    name, ext = splitext(script)
-    newname = renumber(script.name)
-    newscript = script.with_stem(newname)
-    script.rename(newscript)
+    for script in sorted(brocode.glob('*.sql')):
+        print(script.name)
+        name, ext = splitext(script)
+        newname = renumber(script.name)
+        newscript = script.with_stem(newname)
+        script.rename(newscript)
